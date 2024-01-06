5975e4581c67c29fd5721ff7e1ca9ae082f4d9bf
dunewalkin <sergei.gunchenko@gmail.com>
2024-01-06 13:58:24 +0400

    minor edits

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -69,12 +69,12 @@ const Info = ({ userInfo }) => {
             <svg height="18" width="20" xmlns="http://www.w3.org/2000/svg">
                <path d="M20 2.799a8.549 8.549 0 01-2.363.647 4.077 4.077 0 001.804-2.266 8.194 8.194 0 01-2.6.993A4.099 4.099 0 009.75 4.977c0 .324.027.637.095.934-3.409-.166-6.425-1.8-8.452-4.288a4.128 4.128 0 00-.56 2.072c0 1.42.73 2.679 1.82 3.408A4.05 4.05 0 01.8 6.598v.045a4.119 4.119 0 003.285 4.028 4.092 4.092 0 01-1.075.135c-.263 0-.528-.015-.776-.07.531 1.624 2.038 2.818 3.831 2.857A8.239 8.239 0 01.981 15.34 7.68 7.68 0 010 15.285a11.543 11.543 0 006.29 1.84c7.545 0 11.67-6.25 11.67-11.667 0-.182-.006-.357-.015-.53A8.18 8.18 0 0020 2.798z" />
             </svg>
-            <a 
-            className={`twitter-link ${userInfo.twitter_username  !== null ? 'underline-media' : ''}`}
-            href={`https://twitter.com/${userInfo.twitter_username}`|| '#'}
-            target="_blank"  
-            rel="noopener noreferrer" 
-            >   
+            <a
+            className={`twitter-link ${userInfo.twitter_username !== null ? 'underline-media' : 'no-pointer-events'}`}
+            href={`https://twitter.com/${userInfo.twitter_username}` || '#'}
+            target="_blank"
+            rel="noopener noreferrer"
+            >
             {userInfo.twitter_username || 'Not Available'}
             </a>
          </div>
@@ -86,7 +86,7 @@ const Info = ({ userInfo }) => {
                <path d="M13.439 13.75a.401.401 0 00.006-.003c.659-1.204.788-2.586.48-3.933l-.002.002-.001-.001a5.434 5.434 0 00-2.19-3.124.3.3 0 00-.333.015c-.553.448-1.095 1.021-1.452 1.754a.243.243 0 00.096.317c.415.24.79.593 1.04 1.061h.001c.196.33.388.958.263 1.632-.116.894-1.019 1.714-1.736 2.453-.546.559-1.935 1.974-2.49 2.542a2.6 2.6 0 01-3.666.037 2.6 2.6 0 01-.038-3.666l1.521-1.564A.266.266 0 005 11.004c-.338-1.036-.43-2.432-.217-3.51.006-.03-.031-.049-.053-.027l-3.179 3.245c-2.083 2.126-2.066 5.588.04 7.693 2.125 2.083 5.57 2.048 7.653-.078.723-.81 3.821-3.678 4.195-4.577z"/></g>
             </svg>    
             <a
-               className={`website-link ${userInfo.blog !== "" ? 'underline-media' : ''}`}
+               className={`website-link ${userInfo.blog !== "" ? 'underline-media' : 'no-pointer-events'}`}
                href={
                   userInfo.blog &&
                   (userInfo.blog.startsWith('https://') || userInfo.blog.startsWith('http://'))
@@ -106,12 +106,11 @@ const Info = ({ userInfo }) => {
                <path d="M10.858 1.558L1.7.167A1.477 1.477 0 00.517.492 1.49 1.49 0 000 1.608v17.559c0 .458.375.833.833.833h2.709v-4.375c0-.808.65-1.458 1.458-1.458h2.083c.809 0 1.459.65 1.459 1.458V20h3.541V3a1.46 1.46 0 00-1.225-1.442zM4.583 12.292h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm4.167 7.5H7.5a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5H7.5a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5H7.5a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5H7.5a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zM18.85 9.035l-5.933-1.242V20h5.625A1.46 1.46 0 0020 18.542V10.46c0-.688-.47-1.274-1.15-1.425zM16.875 17.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25z"/></g>
             </svg>
             <a 
-            className={`company-link ${userInfo.company !== null ? 'underline-media' : ''}`}
+            className={`company-link ${userInfo.company !== null ? 'underline-media' : 'no-pointer-events'}`}
             href={
                userInfo.company
                ? `https://github.com/${userInfo.company.slice(1)}` || '#'
                : '#'
-               // userInfo.company !== null ? `https://github.com/${userInfo.company.slice(1)}` : '#'
             }
             target="_blank"  
             rel="noopener noreferrer" 
